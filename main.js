cce40045df53bf960759f720e55f1fcf416363ed
Thomas DiMonte <tdimonte@nvisia.com>
2022-06-16 11:07:25 -0500

    Post Challenge

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://cli.vuejs.org/guide/installation.html'
         }
     }
 })
